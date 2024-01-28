50955a5daf63d8f06f2af14e359a759b05527d39
Jaime Alan Gutiérrez Cruz <jgutierrezcruz367@gmail.com>
2024-01-28 03:25:09 -0300

    chore: add Prettier update ESLint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,18 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  plugins: ['@typescript-eslint', 'react', 'jsx-a11y', 'react-hooks', 'react-refresh'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  settings: { react: { version: 'detect' } },
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+  }
+};
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: { open: true }
+  plugins: [react()],
+  server: { open: true }
 });
