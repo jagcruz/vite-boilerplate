df5667bb28121d9b51d3e2a46755563032e9e746
Jaime Alan Gutiérrez Cruz <jgutierrezcruz367@gmail.com>
2024-01-28 03:33:17 -0300

    chore: add `vite-plugin-checker` to Vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,18 @@ import { defineConfig } from 'vite';
 
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    checker({
+      typescript: true,
+      eslint: { lintCommand: 'eslint "./src/**/*.{ts,tsx}" --report-unused-disable-directives' },
+      enableBuild: false
+    })
+  ],
   server: { open: true }
 });
